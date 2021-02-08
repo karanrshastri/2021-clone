a63bdebada32bfa93dcdc285a1db1116e1edaf9b
Karan Shastri <karanrshastri@gmail.com>
2021-02-07 19:49:49 -0500

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,13 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>About Karan</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>Full-Stack User Researcher. My current job revolves around aiding product and design teams make evidence-based decisions by understanding how people read the news.
+	 </p>
+ <p>My current <a href="http://getlighthouse.ca/" target="_blank">side project</a> which I co-work on with four friends involves developing a personalised light-based art product to foster interaction between geographically distant familiy members. 
+	</p>         
+<p>My <a href="https://uwspace.uwaterloo.ca/handle/10012/14917" target="_blank">thesis</a> in grad school focused on understanding the experiences of younger people living with cognitive impairment or dementia and its impact on their vocation.
+	</p>
+<p> Reach out to me via <a href="https://twitter.com/karanrshastri" target="_blank">twitter</a> or email karanrshastri [at]gmail[dot]com.
+	</p>
