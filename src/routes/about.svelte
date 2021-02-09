f6a8b6a6bb9ff4588bcc337f08420311a80e1c1d
Karan Shastri <karanrshastri@gmail.com>
2021-02-08 23:14:44 -0500

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -8,7 +8,7 @@
 	 </p>
  <p>My current <a href="http://getlighthouse.ca/" target="_blank">side project</a> which I co-work on with four friends involves developing a personalised light-based art product to foster interaction between geographically distant familiy members. 
 	</p>         
-<p>My <a href="https://uwspace.uwaterloo.ca/handle/10012/14917" target="_blank">thesis</a> in grad school focused on understanding the experiences of younger people living with cognitive impairment or dementia and its impact on their vocation.
+<p>My <a href="https://uwspace.uwaterloo.ca/handle/10012/14917" target="_blank">thesis</a> in grad school focused on understanding the experiences of younger people living with cognitive impairment or dementia and its impact on their employment.
 	</p>
 <p> Reach out to me via <a href="https://twitter.com/karanrshastri" target="_blank">twitter</a> or email karanrshastri [at]gmail[dot]com.
 	</p>
