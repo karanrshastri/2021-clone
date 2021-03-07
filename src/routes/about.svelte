42b0bccdc27c365d0ff2533f79362c9c7dc97d0a
Karan Shastri <karanrshastri@gmail.com>
2021-03-06 22:31:12 -0500

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,7 +6,7 @@
 
 <p>Full-Stack User Researcher. My current job revolves around aiding product and design teams make evidence-based decisions by understanding how people read the news.
 	 </p>
- <p>My current <a href="http://getlighthouse.ca/" target="_blank">side project</a> which I co-work on with four friends involves developing a personalised light-based art product to foster interaction between geographically distant familiy members. 
+ <p>My current <a href="http://getlighthouse.ca/" target="_blank">side project</a> which I co-work on with four friends involves developing a personalised light-based art product to foster interaction between geographically distant family members. 
 	</p>         
 <p>My <a href="https://uwspace.uwaterloo.ca/handle/10012/14917" target="_blank">thesis</a> in grad school focused on understanding the experiences of younger people living with cognitive impairment or dementia and its impact on their employment.
 	</p>
