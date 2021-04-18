34ba8c763b5d6ec5ed64f9820ff04b3942c12b71
Karan Shastri <karanrshastri@gmail.com>
2021-04-18 13:18:33 -0400

    Update index.svelte

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -25,7 +25,7 @@
 
 <p> You can find my published work from academia <a href="https://scholar.google.com/citations?hl=en&user=3nw4rD4AAAAJ" target="_blank">through Google Scholar</a> 
 	</p>
-<p> Documenting what I've been listening/reading/watching </p>
+<p> Documenting each month</p>
 
 <ul>
 	{#each posts as post}
