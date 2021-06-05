668d3d9e428ed9b954042e396a7a975fc82f1079
Karan Shastri <karanrshastri@gmail.com>
2021-06-04 22:41:57 -0400

    Update _posts.js

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -32,6 +32,7 @@ const posts = [
     title: 'May 2021',
     slug: 'May 2021',
     html: `
+		<p> Started a new job this month which I'm very, very excited about. A lot going on this May, allover. Vaccine (first shot), moving apartments are also major milestones. Feels likes the perfect start to Spring/Summer. Until then. </p>
 		<p>What I've been listening/reading/watching in May 2021.</p>
 
 <ul>
